261938c4ab14030c888a2a40a6d934856981bf8b
JherSter <jeroenavis123@gmail.com>
2017-04-07 13:34:13 +0200

    Timer and max  jump
    ik heb snel een timer gemaakt en een max jumpheight toegevoegd

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 public class Jump : MonoBehaviour
 {
     [SerializeField]
-    private float _jumpStrenght = 300;
+    private float _jumpStrenght;
     private bool _grounded;
     private Rigidbody _rb;
+    [SerializeField]
+    private float _timer;
+
+    [SerializeField] private float _maxJump = 800;
   //private  Vector3 _v = new Vector3(0,-5,0);
 
 
@@ -21,43 +25,53 @@ public class Jump : MonoBehaviour
 
     void Update()
     {
-        print(_grounded);
+
         RaycastHit hit;
         Vector3 downRay = transform.TransformDirection(Vector3.down);
         Debug.DrawRay(transform.position, downRay, Color.black);
-        print("ray: " + Physics.Raycast(transform.position, downRay, out hit, 1));
+
         if (Physics.Raycast(transform.position, downRay, out hit, 1))
         {
-            print("gek");
             if (hit.collider.tag == "Ground")
             {
                 _grounded = true;
-
-                print(hit.collider.tag);
             }
             else
             {
                 _grounded = false;
             }
-
         }
         else
         {
             _grounded = false;
         }
-
+        if (_jumpStrenght > _maxJump)
+        {
+            _jumpStrenght = _maxJump;
+        }
 
         if (Input.GetKey("space"))
         {
-                 _jumpStrenght += 3;
+                _timer++;
+            if (_timer > 100)
+            {
+                _jumpStrenght++;
+
+            }
         }
         if (Input.GetKeyUp("space") && _grounded)
         {
-            _jumpStrenght = 400;
-            _rb.AddForce(0, _jumpStrenght, 0);
+            Jumper();
+            _timer = 0;
         }
+
     }
 
+    private void Jumper()
+    {
+        _rb.AddForce(0, _jumpStrenght, 0);
+        _jumpStrenght = 400;
+    }
 
 
 
