6a4932ba4059685bdc5f0a6bec9ef1f6f928aac0
JherSter <jeroenavis123@gmail.com>
2017-04-13 12:28:10 +0200

    Attack gemaakt + animatie

---
+++ Assets/EnemyBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class EnemyBehaviour : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _death;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hands"))
+        {
+           _death.Invoke();
+        }
+    }
+
+    public void Death()
+    {
+        Destroy(gameObject);
+    }
+}
