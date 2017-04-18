81951bc688fb480b976f24b515422ca78215b1cd
CoenSteuns <c.steuns@gmail.com>
2017-04-18 09:43:25 +0200

    Timer
    Just :clock1:

---
+++ Assets/Scripts/Time/Counter.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+
+public enum CounterMode
+{
+    Up = 1,
+    Down = -1
+}
+
+public class Counter
+{
+    #region Variables
+    #region Properties
+    private float _startTime;
+    private float _currentTime;
+    private float _refreshTime;
+
+    private bool _isPaused;
+
+    private CounterMode _mode;
+    #endregion
+    #region Unity
+    private MonoBehaviour _monoBehaviour;
+    private Coroutine _timerCoroutine;
+    public UnityEvent refreshEvent;
+    #endregion
+    #endregion
+
+    #region Getters&Setters
+    public float StartTime
+    {
+        get { return _startTime; }
+        set { _startTime = value; }
+    }
+    public float CurrentTime
+    {
+        get { return _currentTime; }
+    }
+    public float RefreshTime
+    {
+        get { return _refreshTime; }
+        set { _refreshTime = value; }
+    }
+
+    public bool IsPaused
+    {
+        get { return _isPaused; }
+        set { _isPaused = value; }
+    }
+    public CounterMode Mode
+    {
+        get { return _mode; }
+        set { _mode = value; }
+    }
+    #endregion
+
+    public Counter(float startTime = 0, CounterMode mode = CounterMode.Up, float refreshTime = 1, UnityEvent timedEvent = null)
+    {
+        refreshEvent = timedEvent ?? new UnityEvent();
+        _startTime = startTime;
+        _mode = mode;
+        _refreshTime = refreshTime;
+    }
+
+    public void Start()
+    {
+        if (_monoBehaviour == null)
+        {
+            _monoBehaviour = Object.FindObjectOfType<MonoBehaviour>();
+            if (_monoBehaviour == null)
+            {
+                Debug.Log("No monobehavior in scene!");
+                return;
+            }
+        }
+        _timerCoroutine = _monoBehaviour.StartCoroutine(TimerCoroutine());
+    }
+
+    public void Stop()
+    {
+        _monoBehaviour.StopCoroutine(_timerCoroutine);
+        _currentTime = _startTime;
+    }
+
+    public void Reset(bool stopTimer = false)
+    {
+        _currentTime = _startTime;
+        if (stopTimer)
+        {
+            Stop();
+        }
+    }
+
+    public void SetMonobehavior(MonoBehaviour mono)
+    {
+        _monoBehaviour = mono;
+    }
+
+    private IEnumerator TimerCoroutine()
+    {
+        _currentTime = _startTime;
+        while (true)
+        {
+            while (!_isPaused)
+            {
+                _currentTime += _refreshTime * (int)_mode;
+                refreshEvent.Invoke();
+                yield return new WaitForSeconds(_refreshTime);
+            }
+            yield return null;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Time/TimerTester.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimerTester : MonoBehaviour
+{
+
+
+  /*  public Counter cOne;
+    public Counter cTwo;
+
+    public TimeSpan t;
+
+    // Use this for initialization
+    void Start () {
+        cOne = new Counter();
+        cOne.SetMonobehavior(this);
+        cOne.refreshEvent.AddListener(NewTimeSpan);
+        cOne.Start();
+
+
+    }
+
+    void NewTimeSpan()
+    {
+        t = TimeSpan.FromSeconds(cOne.CurrentTime);
+        print(t.ToString());
+        
+
+    }*/
+
+}
